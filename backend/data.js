2d399442e8b0c233abfe05aaae550c15bb59a58e
nuwan <nuwancs@gmail.com>
2019-06-30 16:34:33 +0530

    intial commit 2019-June

---
+++ backend/data.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const DataSchema = new Schema(
+        {
+                id: Number,
+                message: String
+        },
+        { timestamps: true }
+);
+
+module.exports = mongoose.model("Data", DataSchema);
+
+
+
+
+
+
+
+
+
+
